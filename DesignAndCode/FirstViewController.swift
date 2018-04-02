6c5e56d07959390674e53d32db23864e117b3051
Tiago Silva <1140437@isep.ipp.pt>
2018-04-02 19:52:18 +0100

    Initial Commit

---
+++ DesignAndCode/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DesignAndCode
+//
+//  Created by Tiago Santos on 02/04/18.
+//  Copyright © 2018 Tiago Santos. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
