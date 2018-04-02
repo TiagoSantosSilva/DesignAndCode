c06d37fd1ed40b976105067ec1a69de76e1e0eeb
Tiago Santos <tiagossilva_95@hotmail.com>
2018-04-02 21:15:46 +0100

    Apple UI Colors

---
+++ DesignAndCode/Context/Shared/Extensions/UIKit/UIColor.swift
@@ -0,0 +1,26 @@
+//
+//  UIColor.swift
+//  DesignAndCode
+//
+//  Created by Tiago Santos on 02/04/18.
+//  Copyright © 2018 Tiago Santos. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Apple Colors
+
+extension UIColor {
+    static let appleBackgroundGray = UIColor(red: 239/255, green: 239/255, blue: 239/255, alpha: 1)
+    static let appleBlue = UIColor(red: 0, green: 122/255, blue: 1, alpha: 1)
+    static let appleDarkGray = UIColor(red: 142/255, green: 142/255, blue: 147/255, alpha: 1)
+    static let appleDeepBlue = UIColor(red: 0, green: 122/255, blue: 1, alpha: 1)
+    static let appleGreen = UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1)
+    static let appleLightBlue = UIColor(red: 90/255, green: 200/255, blue: 250/255, alpha: 1)
+    static let appleLinesGray = UIColor(red: 206/255, green: 206/255, blue: 210/255, alpha: 1)
+    static let appleMagenta = UIColor(red: 1, green: 45/255, blue: 85/255, alpha: 1)
+    static let appleOrange = UIColor(red: 1, green: 149/255, blue: 0, alpha: 1)
+    static let applePurple = UIColor(red: 88/255, green: 86/255, blue: 214/255, alpha: 1)
+    static let appleRed = UIColor(red: 1, green: 59/255, blue: 48/255, alpha: 1)
+    static let appleYellow = UIColor(red: 1, green: 204/255, blue: 0, alpha: 1)
+}
